26e3dacb8f832853199fd2ea565177bad6256341
Peefy <xpf6677@163.com>
2019-11-03 14:30:01 +0800

    Update

--- src/main.rb
+++ src/main.rb
@@ -156,4 +156,29 @@ def test
 end
 test {|i| puts "在块#{i}内"}
 
-
+module A
+    def a1
+    end
+    def a2
+    end
+ end
+ module B
+    def b1
+    end
+    def b2
+    end
+ end
+  
+ class Sample
+ include A
+ include B
+    def s1
+    end
+ end
+  
+ samp=Sample.new
+ samp.a1
+ samp.a2
+ samp.b1
+ samp.b2
+ samp.s1
